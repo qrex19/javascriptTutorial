8ac75f46618e8f245575efeb5c54702598768eb5
qrex19 <nishanttjhaa25@gmail.com>
2023-10-28 22:59:04 +0530

    improv: learnt about conversions

--- dataTypeConversionAndOperations.js
+++ dataTypeConversionAndOperations.js
@@ -28,3 +28,43 @@ let someNumber = 23
 let stringNumber = String(someNumber)
 
 console.log(typeof stringNumber)
+
+// ************************* Operationd ***********************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2 + 2)
+// console.log(2**2) //power
+
+let str1 = 'hello'
+let str2 = ' hello'
+
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2) //string + number => string
+// console.log(1 + '2')
+// console.log("1" + "2")
+
+// console.log('1' + 2 + 2)
+// console.log(1 + 2 + '2') // number + number => number + string => string
+// console.log((2 + 4) / 5)
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+console.log(num1)
+console.log(num2)
+console.log(num3)
+
+let gameCounter = 100;
+
+// gameCounter++;
+// ++gameCounter
+
+console.log(++gameCounter)
