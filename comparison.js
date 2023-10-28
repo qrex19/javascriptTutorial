e8b8e50aa7fa8eeeb38ac011c78298887bf9257f
qrex19 <nishanttjhaa25@gmail.com>
2023-10-28 23:07:06 +0530

    improv: comparison file added

---
+++ comparison.js
@@ -0,0 +1,15 @@
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 != 1)
+
+console.log('2' > 1)
+console.log('1' > 2) //javascript automatically converts the string into a number
+
+console.log(null > 0) //causes confusion
+
+console.log(undefined == 0)
+
+// ===
+
+console.log('2' == 2)
+console.log('2' === 2) //returns false, cus === checks the datatype as well
