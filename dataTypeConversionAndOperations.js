a80fbfb98e6622319c22f8d3a2cf986d7514ecb3
qrex19 <nishanttjhaa25@gmail.com>
2023-10-28 17:48:47 +0530

    improv: datatypeConversion file added

---
+++ dataTypeConversionAndOperations.js
@@ -0,0 +1,30 @@
+let score = '33'
+
+
+// console.log(typeof score) //returns number
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber) //typeof can be used in both the ways
+// console.log(valueInNumber) //prints NaN(Not a Number)
+
+//"33" => 33
+// "33abd" => NaN
+// true => 1
+//false => 0
+
+let isLoggedIn = "nishant"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+//1 => ture
+// 0 => false
+// "" => false
+// "nishant" => true
+
+let someNumber = 23
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
