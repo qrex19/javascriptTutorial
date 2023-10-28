5747aaf5a9d05f23c4ee6d009853ca4e6539b44b
qrex19 <nishanttjhaa25@gmail.com>
2023-10-28 17:34:30 +0530

    formatted using prettier

--- variables.js
+++ variables.js
@@ -1,15 +1,15 @@
-const accountId = 144553
-let accoutEmail = 'nishanttjhaa25@gmail.com'
-var accountPassword = 'password@123'
-accountCity = 'jaipur' //don't use this way of initialization and declaration
+const accountId = 144553;
+let accoutEmail = "nishanttjhaa25@gmail.com";
+var accountPassword = "password@123";
+accountCity = "jaipur"; //don't use this way of initialization and declaration
 
-let accountState
+let accountState;
 
 // accountId = 2
 
-console.log(accountId)
+console.log(accountId);
 
 // Prefer not to use var, because of issues in scoping
 
-console.table([accountId, accoutEmail, accountPassword])
-console.log(accountState)
+console.table([accountId, accoutEmail, accountPassword]);
+console.log(accountState);
