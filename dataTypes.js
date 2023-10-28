7a8067a647a7b2403639d5f3eac3227a067356df
qrex19 <nishanttjhaa25@gmail.com>
2023-10-28 17:34:54 +0530

    improv: dataTypes file added

---
+++ dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+// alert(2 + 2) //we are using node.js not browser
+
+//Code readibility must be high
+
+let name = 'nishant'
+let age = 18
+let isLoggedIn = true
+let state;
+
+// number range is 2 to power 53
+// bigint
+//string=> ""
+//boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null) //prints 'Object' because null is an object
+
