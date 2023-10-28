0c3c3ef130dcbab058e619a396f1cd5e269da20e
qrex19 <nishanttjhaa25@gmail.com>
2023-10-28 17:19:49 +0530

    improv: variables file added

---
+++ variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accoutEmail = 'nishanttjhaa25@gmail.com'
+var accountPassword = 'password@123'
+accountCity = 'jaipur' //don't use this way of initialization and declaration
+
+let accountState
+
+// accountId = 2
+
+console.log(accountId)
+
+// Prefer not to use var, because of issues in scoping
+
+console.table([accountId, accoutEmail, accountPassword])
+console.log(accountState)
