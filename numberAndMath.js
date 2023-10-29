4c7793bf24991afde2f70685d97e9e35ff2382e4
qrex19 <nishanttjhaa25@gmail.com>
2023-10-29 13:02:39 +0530

    improv: Math file added

---
+++ numberAndMath.js
@@ -0,0 +1,26 @@
+let score = 340
+
+// console.log(score)
+
+let balance = 340.78654
+
+// console.log(balance.toFixed(3)) //take the ceiling value of the 3rd decimal place
+
+const otherNumber = 23.899
+
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// console.log(Number.MIN_VALUE)
+// console.log(Number.MAX_VALUE)
+
+//**************************************************************** */
+
+// console.log(Math.abs(-5))
+// console.log(Math.ceil(4.3))
+// console.log(Math.min(1, 2, 3, 4, 5, 6, 7, 8))
+
+console.log(Math.floor((Math.random() * 10) + 1))
